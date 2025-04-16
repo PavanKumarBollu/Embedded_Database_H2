fb00fef9889446115855ecb68a5664870b093443
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-17 01:26:36 +0530

    Working with the H2 Database

--- 01_EmbeddedDataBase_H2/src/main/java/com/pavan/service/StudentServiceImpl.java
+++ 01_EmbeddedDataBase_H2/src/main/java/com/pavan/service/StudentServiceImpl.java
@@ -2,11 +2,14 @@ package com.pavan.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.pavan.dao.StudentDao;
 import com.pavan.model.Student;
 
 public class StudentServiceImpl implements IStudentService {
 
+	@Autowired
 	private StudentDao dao;
 	
 	
