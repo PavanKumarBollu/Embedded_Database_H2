29d63fc3de9fc4017295cd9f54d9e322483069f9
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-17 01:38:10 +0530

    Working with the H2 Database

--- 01_EmbeddedDataBase_H2/src/main/java/com/pavan/restcontroller/StudentRestController.java
+++ 01_EmbeddedDataBase_H2/src/main/java/com/pavan/restcontroller/StudentRestController.java
@@ -2,8 +2,12 @@ package com.pavan.restcontroller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pavan.model.Student;
 
 @RestController
 @RequestMapping(value="/student")
