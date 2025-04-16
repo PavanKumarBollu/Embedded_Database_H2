38c09e49b0349ff551d1e01dac7ad726965e1f16
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-17 01:23:38 +0530

    Working with the H2 Database

--- 01_EmbeddedDataBase_H2/src/main/java/com/pavan/dao/StudentDao.java
+++ 01_EmbeddedDataBase_H2/src/main/java/com/pavan/dao/StudentDao.java
@@ -2,6 +2,8 @@ package com.pavan.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.pavan.model.Student;
+
 public interface StudentDao extends JpaRepository<Student, Integer> {
 
 }
