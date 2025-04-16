8303d7efa45dd3f2680d01ff85b72f826ae5a2f3
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-17 01:20:36 +0530

    Working with the H2 Database

--- 01_EmbeddedDataBase_H2/src/main/java/com/pavan/handler/GlobalExceptionHandler.java
+++ 01_EmbeddedDataBase_H2/src/main/java/com/pavan/handler/GlobalExceptionHandler.java
@@ -3,14 +3,17 @@ package com.pavan.handler;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.pavan.error.ErrorDetails;
 import com.pavan.exeception.StudentRecordNotFoundException;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(StudentRecordNotFoundException.class)
 	public ResponseEntity<ErrorDetails> studentNotFound(StudentRecordNotFoundException srnfe) {
 		return new ResponseEntity<ErrorDetails>(
 				new ErrorDetails(LocalDateTime.now(), srnfe.getMessage(), "Record Not found"), HttpStatus.OK);
