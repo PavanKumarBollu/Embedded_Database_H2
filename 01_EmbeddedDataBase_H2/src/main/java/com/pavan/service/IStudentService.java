89bb072ae3d4c05e01bd7281667a256f2d999d4c
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-17 01:24:25 +0530

    Working with the H2 Database

--- 01_EmbeddedDataBase_H2/src/main/java/com/pavan/service/IStudentService.java
+++ 01_EmbeddedDataBase_H2/src/main/java/com/pavan/service/IStudentService.java
@@ -2,4 +2,13 @@ package com.pavan.service;
 
 public interface IStudentService {
 
+	public String saveStudent(Student student);
+
+	public List<Student> findAllStudents();
+
+	public Student findById(Integer id);
+
+	public String updateStudent(Student student);
+
+	public String deleteById(Integer id);
 }
