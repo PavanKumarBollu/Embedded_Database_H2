93eab29be83c2f85856271612050d473c69f187e
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-17 01:12:30 +0530

    Working with the H2 Database

--- 01_EmbeddedDataBase_H2/src/main/java/com/pavan/error/ErrorDetails.java
+++ 01_EmbeddedDataBase_H2/src/main/java/com/pavan/error/ErrorDetails.java
@@ -1,5 +1,13 @@
 package com.pavan.error;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDetails {
 	private LocalDateTime ldt;
 	private String errorMsg;
