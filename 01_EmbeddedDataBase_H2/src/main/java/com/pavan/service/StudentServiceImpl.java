f403259ac83b5ee11d919e900ebe9d5f2f1c4e18
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-17 01:25:21 +0530

    Working with the H2 Database

---
+++ 01_EmbeddedDataBase_H2/src/main/java/com/pavan/service/StudentServiceImpl.java
@@ -0,0 +1,39 @@
+package com.pavan.service;
+
+import java.util.List;
+
+import com.pavan.model.Student;
+
+public class StudentServiceImpl implements IStudentService {
+
+	@Override
+	public String saveStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> findAllStudents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
